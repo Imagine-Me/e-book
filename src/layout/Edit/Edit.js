335cb584367a6e0500329ad7879eb347d2498d27
Imagine-Me <prince100thomas@gmail.com>
2021-01-23 12:09:57 +0530

    Add rich text editor

--- src/layout/Edit/Edit.js
+++ src/layout/Edit/Edit.js
@@ -1,10 +1,31 @@
+import { useState } from 'react'
+
 import { Form } from "react-bootstrap"
 
+import { EditorState } from 'draft-js'
+import { Editor } from 'react-draft-wysiwyg';
+
+
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
 const Edit = () => {
+    const [editorState, setEditorState] = useState(() => EditorState.createEmpty())
+
+
     return <div className="p-3">
         <form>
             <Form.Label>Page Title</Form.Label>
             <Form.Control type="text" placeholder="Page Title" />
+            <div className="editor-wrapped">
+                <Editor
+                    editorStyle={{ height: '240px', padding: '0 15px' }}
+                    editorState={editorState}
+                    onEditorStateChange={setEditorState}
+                    wrapperClassName="wrapper-class"
+                    editorClassName="editor-class"
+                    toolbarClassName="toolbar-class"
+                />
+            </div>
         </form>
     </div>
 }

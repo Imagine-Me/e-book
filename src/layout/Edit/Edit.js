68a643fe12d01e5136e399a36f24534d88f08b21
Imagine-Me <prince100thomas@gmail.com>
2021-01-23 13:48:47 +0530

    Add richtext editor related feat

--- src/layout/Edit/Edit.js
+++ src/layout/Edit/Edit.js
@@ -1,21 +1,30 @@
 import { useState } from 'react'
 
-import { Form } from "react-bootstrap"
+import { Form, Button } from "react-bootstrap"
 
 import { EditorState } from 'draft-js'
 import { Editor } from 'react-draft-wysiwyg';
 
 
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { convertToHTML } from 'draft-convert';
 
 const Edit = () => {
     const [editorState, setEditorState] = useState(() => EditorState.createEmpty())
+    const [pageTitle, setPageTitle] = useState('')
+
+    const onSubmitForm = (event) => {
+        event.preventDefault()
+        console.log(convertToHTML(editorState.getCurrentContent()));
+    }
 
 
     return <div className="p-3">
-        <form>
+        <form onSubmit={onSubmitForm}>
+            <div>
+            </div>
             <Form.Label>Page Title</Form.Label>
-            <Form.Control type="text" placeholder="Page Title" />
+            <Form.Control type="text" placeholder="Page Title" onChange={(event) => setPageTitle(event.target.value)} value={pageTitle} required />
             <div className="editor-wrapped">
                 <Editor
                     editorStyle={{ height: '240px', padding: '0 15px' }}
@@ -26,6 +35,7 @@ const Edit = () => {
                     toolbarClassName="toolbar-class"
                 />
             </div>
+            <Button variant="primary" type="submit">Submit</Button>
         </form>
     </div>
 }

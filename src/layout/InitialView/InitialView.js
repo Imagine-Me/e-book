6a8f8dfd9011a718ef7bcef0fe664b736eca0e8b
Imagine-Me <prince100thomas@gmail.com>
2021-01-23 20:30:15 +0530

    Add Initial page

--- src/layout/InitialView/InitialView.js
+++ src/layout/InitialView/InitialView.js
@@ -1,5 +1,8 @@
 import { Link } from 'react-router-dom'
 
 export default function InitialView() {
-    return <Link to="/create">Create new</Link>
+    return <div className="mt-5 text-center">
+        <h1>There are no pages added</h1>
+        <Link to="/create" className="btn btn-primary mt-5">Create new</Link>
+    </div>
 }
